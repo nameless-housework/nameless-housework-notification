2207236e6f04c7ec7a5cd1b08a0fc2207228738c
waricoma <motone.adachi@3n38.app>
2023-01-30 02:18:38 +0900

    :rocket:  add: deploy config for github pages

--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-input v-model="input" placeholder="Please input" />
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const input = ref('');
+</script>
