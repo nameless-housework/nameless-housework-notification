4304604f1e07481c34919e9c64b623b520079ca9
Motone Adachi <motone.adachi@nightlamp.co.jp>
2023-01-30 22:52:34 +0900

    :sparkles:  add: ngrok auto setup (#6) (#7)
    * :sparkles:  add: ngrok auto setup
    
    * :recycle:  refactor: web gui url

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import { requestI2CAccess } from 'node-web-i2c';
-import NPIX from '@chirimen/neopixel-i2c';
-import ambient from 'ambient-lib';
+import http from 'http';
 
-console.log('Hello world');
+import express from 'express';
+import ngrok from 'ngrok';
+// import { requestI2CAccess } from 'node-web-i2c';
+// import NPIX from '@chirimen/neopixel-i2c';
+// import ambient from 'ambient-lib';
+
+import { PORT, NGROK_AUTH_TOKEN, WEB_GUI_URL } from './constants/constant';
+
+const SYSTEM = { ngrokUrl: '' };
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.redirect(`${WEB_GUI_URL}/?api=${SYSTEM.ngrokUrl}`);
+});
+
+(async () => {
+  http.createServer(app).listen(PORT, async () => {
+    SYSTEM.ngrokUrl = await ngrok.connect({ authtoken: NGROK_AUTH_TOKEN, addr: PORT });
+    console.log(SYSTEM.ngrokUrl);
+  });
+})();
--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -2,10 +2,12 @@
   <div class="about">
     <h1>This is an about page</h1>
     <el-input v-model="input" placeholder="Please input" />
+    <el-button type="success" :icon="Check" circle />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { Check } from '@element-plus/icons-vue';
 const input = ref('');
 </script>
